db15e74c87057a22a6f22c9a9c2f07fbbc2e90c1
Jessica Lin <66900456+jessicalin94@users.noreply.github.com>
2023-11-21 13:39:09 +1100

    add read me

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,7 +6,10 @@ import (
 
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	//	TODO: Implement this...
-	return nil
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
 }
 
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
